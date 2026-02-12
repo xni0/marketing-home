9fe440e6cbda9f31ed7bff683e76e983234a8c57
xni0 <alumno.206012@ies-azarquiel.es>
2026-02-13 00:41:53 +0100

    Arreglando errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Servicios from './pages/Servicios';
